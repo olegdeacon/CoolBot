6bd17f1e47ecab045ec7fa43dba6473c2313dadd
olegdeacon <o_deacon@hotmail.com>
2021-08-18 19:45:11 -0600

    Wed, Aug 18, 19:45:10

---
+++ getDate.js
@@ -0,0 +1,8 @@
+let today = new Date()
+let date = new Date().toLocaleDateString()
+let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+console.log(date)
+console.log(time)
+
+
